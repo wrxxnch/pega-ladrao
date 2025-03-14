399cfe4032027d6014ebe4e1e65b55665ccf8124
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2025-03-14 02:40:32 -0400

    add router ads imgs

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,28 @@ const appName = import.meta.env.VITE_APP_NAME;
 </script>
 
 <template>
-  <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-light bg-light mb-3">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
         {{ appName }}
       </a>
     </div>
   </nav>
+  <main>
+    <div class="container">
+      <RouterView />
+    </div>
+  </main>
+  <footer>
+
+    <nav class="navbar navbar-light bg-light mb-3">
+      <div class="container-fluid">
+        <span class="navbar-text" style="margin-left: auto;margin-right: auto;">
+          © 2024 <b>DevRand0m</b> | All rights reserved.
+        </span>
+      </div>
+    </nav>
+  </footer>
 </template>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Crie Mensagens Secretas Fácil</h1>
+    <hr>
+    <div class="card mb-3">
+        <div class="card-body card-text">
+            ✅ Por meio deste aplicativo você pode criar suas próprias mensagens criptografadas e envia-las para seus
+            amigos ou guardar de forma segura. Abaixo você pode criar uma mensagem super rápido! 🚀
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="msgBruta" class="form-label">📜 Digite aqui sua mensagem ou texto</label>
+        <textarea class="form-control" id="msgBruta" rows="5"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="msgKey" class="form-label">🔑 Chave secreta</label>
+        <input type="text" class="form-control" id="msgKey" placeholder="Sp1k3" maxlength="20">
+    </div>
+    <div class="mb-3">
+        <label for="msgTip" class="form-label">💡 Dica da chave secreta</label>
+        <input type="text" class="form-control" id="msgTip" maxlength="50" placeholder="Meu primeiro pet">
+    </div>
+    <div class="mb-3 text-center">
+        <button type="button" class="btn btn-success btn-lg">Criptografar 🥷</button>
+    </div>
+    <br>
+    <div class="mb-3">
+        <label for="msgEnc" class="form-label">Resultado</label>
+        <textarea class="form-control" id="msgEnc" rows="5" readonly></textarea>
+    </div>
+    <div class="mb-3 text-center">
+        <button type="button" class="btn btn-info btn-lg">Copiar</button>
+    </div>
+</template>
