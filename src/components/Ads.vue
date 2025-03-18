a53f1df65a8ba2b1683f1909b2a0ef859eedb218
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2025-03-18 01:26:33 -0400

    add urls ads

--- src/components/Ads.vue
+++ src/components/Ads.vue
@@ -1,16 +1,45 @@
 <template>
     <div class="mb-3 text-center">
-        <img :src="imgSrc" class="img-fluid ads">
+        <a :href="data.urls[data.randomNum - 1]" target="_blank" rel="noopener noreferrer">
+            <img :src="imgSrc" class="img-fluid ads">
+        </a>
     </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
 
-const randomNum = Math.floor(Math.random() * 23) + 1;
+const data = reactive({
+    urls: [
+        'https://cassino.bet.br/',
+        'https://www.canva.com/pt_br/',
+        'https://www.bemol.com.br/',
+        'https://www.instagram.com/bis',
+        'https://www.instagram.com/bis',
+        'https://www.coca-cola.com/br/pt',
+        'https://usefathom.com/',
+        'https://www.bemol.com.br/',
+        'https://www.vw.com.br/pt.html',
+        'https://www.pravaler.com.br/',
+        'https://www.instagram.com/oreo_brasil/',
+        'https://cassino.bet.br/',
+        'https://www.instagram.com/bis',
+        'https://www.instagram.com/oreo_brasil/',
+        'https://www.cruzeirodosul.edu.br/',
+        'https://www.canva.com/pt_br/',
+        'https://www.instagram.com/trident_brasil/',
+        'https://cassino.bet.br/',
+        'https://www.hoteis.com/',
+        'https://www.cloudtalk.io/pt-br/',
+        'https://darklabsuplementos.com.br/',
+        'https://www.texaco.com.br/',
+        'https://www.canva.com/pt_br/',
+    ],
+    randomNum: Math.floor(Math.random() * 23) + 1
+});
 
 const imgSrc = computed(() => {
-    return new URL(`../assets/ads/${randomNum}.png`, import.meta.url).href
+    return new URL(`../assets/ads/${data.randomNum}.png`, import.meta.url).href
 });
 </script>
 
