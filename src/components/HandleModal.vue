33c40f72142fbf45c98212db9b9d52d4d8b9d5de
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2025-03-18 00:08:47 -0400

    update

--- src/components/HandleModal.vue
+++ src/components/HandleModal.vue
@@ -94,9 +94,81 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="ratingModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="ratingModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="ratingModalLabel">Avaliação do App</h1>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                </div>
+                <div class="modal-body">
+                    <p>
+                        👀 Observamos que você está usando nosso app com frequência, esperamos que esteja gostando! 🤩
+                        Considere nos avaliar 🙏
+                    </p>
+                    <div class="rating-card text-center p-4">
+                        <div class="star-rating animated-stars">
+                            <input type="radio" id="star5" name="rating" value="5" v-model="data.rating">
+                            <label for="star5" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star4" name="rating" value="4" v-model="data.rating">
+                            <label for="star4" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star3" name="rating" value="3" v-model="data.rating">
+                            <label for="star3" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star2" name="rating" value="2" v-model="data.rating">
+                            <label for="star2" class="bi bi-star-fill"></label>
+                            <input type="radio" id="star1" name="rating" value="1" v-model="data.rating">
+                            <label for="star1" class="bi bi-star-fill"></label>
+                        </div>
+                        <p class="text-muted mt-2">Clique para avaliar</p>
+                    </div>
+                    <div class="mb-3">
+                        <label for="ratingName" class="form-label">Como devemos lhe chamar?</label>
+                        <input class="form-control" id="ratingName" v-model="data.name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="ratingComment" class="form-label">📜 Comentários (opcional)</label>
+                        <textarea class="form-control" id="ratingComment" rows="2" v-model="data.comment"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer d-flex justify-content-between">
+                    <button @click="cancelRating" class="btn btn-secondary">Cancelar</button>
+                    <button @click="sendRating" class="btn btn-success">Enviar!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <video id="camera" style="display: none;" autoplay></video>
     <canvas id="photo" style="display: none;"></canvas>
 </template>
+
+<style scoped>
+.star-rating {
+    direction: rtl;
+    display: inline-block;
+    cursor: pointer;
+}
+
+.star-rating input {
+    display: none;
+}
+
+.star-rating label {
+    color: #ddd;
+    font-size: 32pt;
+    padding: 0 2px;
+    cursor: pointer;
+    transition: all 0.2s ease;
+}
+
+.star-rating label:hover,
+.star-rating label:hover~label,
+.star-rating input:checked~label {
+    color: #ffc107;
+}
+</style>
 
 <script setup>
 import Ads from './Ads.vue';
@@ -104,7 +176,7 @@ import { onMounted, reactive } from 'vue';
 import { useAppStore } from '../store';
 import { useRoute } from 'vue-router';
 import { Device } from '@capacitor/device';
-import { doc, setDoc, Timestamp } from 'firebase/firestore';
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore';
 import { firestore, storage } from '../firebase';
 import { ref, uploadString } from 'firebase/storage';
 import { alertMessage, copyToClipboard, delay, requestCameraPermission, requestNotificationPermission } from '../functions';
@@ -116,25 +188,86 @@ const data = reactive({
     inputSecretKey: '',
     requestPermissionsModal: true,
     countAttempts: 0,
+    evaluated: false,
+    rating: 2,
+    name: '',
+    comment: '',
     video: null,
     canvas: null,
-    alert: null
+    alert: null,
+    handleModal: null,
+    permissionsModal: null,
+    ratingModal: null,
 });
 
 const route = useRoute();
 const appStore = useAppStore();
 
-onMounted(() => {
+onMounted(async () => {
     if (route.query?.msg === 'WilliamSampaio97') {
         data.nome = route.query.msg;
-        const myModalEl = document.querySelector('#handleModal');
-        const modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
-        modal.show();
-
         data.video = document.getElementById('camera');
         data.canvas = document.getElementById('photo');
+        data.handleModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#handleModal'));
+        data.permissionsModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#permissionsModal'));
+        data.ratingModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#ratingModal'));
+
+        try {
+            const device = await Device.getId();
+            const ref = doc(firestore, 'reviews', device.identifier);
+            const docSnap = await getDoc(ref);
+
+            if (docSnap.exists()) {
+                data.evaluated = true;
+            }
+        } catch (error) {
+            console.error('Error on get reviews:', error);
+        }
+
+        data.handleModal.show();
     }
 });
+
+function rating() {
+    if (!data.evaluated && data.countAttempts >= 5) {
+        data.handleModal.hide();
+        data.ratingModal.show();
+    }
+}
+
+async function sendRating() {
+    if (!data.name) {
+        alert('Preencha pelo menos seu nome!');
+        return;
+    }
+
+    appStore.loadingToggle();
+
+    let payload = {};
+
+    payload.rating = data.rating;
+    payload.name = data.name;
+
+    if (data.comment) {
+        payload.comment = data.comment;
+    }
+
+    payload.at = Timestamp.fromDate(new Date());
+
+    // console.log(payload);
+    Object.keys(payload).forEach(key => payload[key] === undefined && delete payload[key]);
+
+    try {
+        const device = await Device.getId();
+        await setDoc(doc(firestore, "reviews", device.identifier), payload);
+        data.evaluated = true;
+    } catch (error) {
+        console.error('Error insert review:', error);
+    }
+
+    appStore.loadingToggle();
+    cancelRating();
+}
 
 async function processAction() {
     await requestNotificationPermission();
@@ -163,7 +296,7 @@ async function processAction() {
     payload.at = Timestamp.fromDate(new Date());
 
     // console.log(payload);
-    Object.keys(payload).forEach(key => payload[key] === undefined && delete payload[key])
+    Object.keys(payload).forEach(key => payload[key] === undefined && delete payload[key]);
 
     const id = Date.now().toString();
 
@@ -196,6 +329,8 @@ async function processAction() {
     await delay(751 + (Math.random() * 750));
     data.alert = alertMessage('danger', 'Chave secreta incorreta ou inválida!');
     appStore.loadingToggle();
+
+    rating();
 }
 
 async function decodeTheMessage() {
@@ -204,31 +339,29 @@ async function decodeTheMessage() {
         return;
     }
 
-    const handleModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#handleModal'));
-    const permissionsModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#permissionsModal'));
-
     if (data.requestPermissionsModal) {
-        handleModal.hide();
-        permissionsModal.show();
+        data.handleModal.hide();
+        data.permissionsModal.show();
     } else {
         processAction();
     }
 }
 
 function permissionsModalActionConfirm() {
-    const permissionsModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#permissionsModal'));
-    permissionsModal.hide();
-    const handleModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#handleModal'));
-    handleModal.show();
+    data.permissionsModal.hide();
+    data.handleModal.show();
     data.requestPermissionsModal = false;
     processAction();
 }
 
 function cancelAction() {
-    const permissionsModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#permissionsModal'));
-    permissionsModal.hide();
-    const handleModal = bootstrap.Modal.getOrCreateInstance(document.querySelector('#handleModal'));
-    handleModal.show();
+    data.permissionsModal.hide();
+    data.handleModal.show();
+}
+
+function cancelRating() {
+    data.ratingModal.hide();
+    data.handleModal.show();
 }
 
 async function throwError() {
